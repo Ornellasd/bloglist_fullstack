0adf9404d39d3008bcded03070faafd75d082b3b
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-06-09 14:45:16 +0200

    initialize loginForm

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -4,6 +4,8 @@ import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +13,39 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log(`Logging in ${username} with ${password}`)
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)} 
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
   return (
     <div>
       <h2>blogs</h2>
+      {loginForm()}
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
