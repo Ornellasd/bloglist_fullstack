b38628f6da35a08ea35135373c7dbfa38f1f792a
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-06-16 13:24:40 +0200

    finish 5.16 - test that BlogForm calls event handler with right details

--- bloglist-frontend/src/components/Blog.test.js
+++ bloglist-frontend/src/components/Blog.test.js
@@ -1,22 +1,27 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent } from '@testing-library/react'
-import { prettyDOM } from '@testing-library/dom'
 import Blog from './Blog'
-import Togglable from './Togglable'
 
 describe('render tests', () => {
+  const user = {
+    username: 'testy',
+    id: '123456789'
+  }
+
   const blog = {
     title: 'Test Blog',
     author: 'Testy McTestFace',
-    url: 'http://www.test.com'
+    url: 'http://www.test.com',
+    likes: 60,
+    user: user
   }
 
   let component
 
   beforeEach(() => {
     component = render(
-      <Blog blog={blog} />
+      <Blog blog={blog} user={user} />
     )
   })
 
@@ -30,7 +35,6 @@ describe('render tests', () => {
     const button = component.getByText('view')
     fireEvent.click(button)
 
-    const div = component.container.querySelector('.expanded')
     expect(
       component.container.querySelector('.expanded')
     ).toBeDefined()
